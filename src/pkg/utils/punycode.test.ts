47657fff5dfdcc1bcb69d3eaaeb7eea51881a008
Soulock <1499230087@qq.com>
2026-04-07 11:48:33 +0800

    test: update punycode fixtures for supercat

--- src/pkg/utils/punycode.test.ts
+++ src/pkg/utils/punycode.test.ts
@@ -18,13 +18,13 @@ describe.concurrent("punycode - decode only", () => {
     expect(decodePunycode("xn--ba-lmcq")).toBe("bрфa");
 
     const codes = {
-      "为什么选择supercat-超级猫": "xn--supercat--xx2pif85dpx1n4mn5i9brrzc1f2c",
+      "为什么选择supercat-超级猫": "xn--supercat--fp6n0ex3d859lcu6b889a4yzcr6i",
       "supercat超级猫完全兼容油猴脚本-同时提供后台脚本运行框架-丰富的api扩展-让你的浏览体验更出色":
-        "xn--supercat--api--803xq5lxg84bn7x9law6gwxrftatbw476a18aopi2ky56dycj3jr6wh00beah022clsg5u6cb6en53e7ka292jha3303jiai546nxt1eksp1sw6k3c251h",
+        "xn--supercat--api--zs7vh5lkg32bh4wplaq2gk1qmsarbw735ar7a4o07ji34dp1iexqxphjzbha449bd7f213cqxexo3epkal31jha3560gpswaia759mc50eu5okwvchv5x0aky7j",
       "为什么选择supercat-基于油猴的设计理念-完全兼容油猴脚本-提供更多丰富的api让脚本能够完成更多强大的功能":
-        "xn--supercat---api-903xremky1ci2a25kznmfnap9t8q1beegea3l0js49dka54lbs864gsvf33vulhm9i656aja69tka9912dga2675eha679g784aral7387kja72mma6139lpna01bz17n",
+        "xn--supercat---api-0s7vqewj10c30as9jt8lumaj7t239a54fea8ksj017dka00lrru91ggqfpovofhz2i795aja04tka8770dga3014eha667gzq4aral6515kja89lma1266lhnam2bm30p",
       "为什么选择supercat-基于油猴的设计理念-完全兼容油猴脚本-提供更多丰富的api-超级猫不仅兼容油猴脚本-还支持后台脚本运行-功能更强大-覆盖范围广-安装脚本管理器-寻找适合的脚本一键安装即可":
-        "xn--supercat---api-------ql07anix3aghnnp9dxybze30yj7pksaja010heqhtxe3a13grg250g79e2zm98lzqas99fea7iu6hla65gc3exv9ccdi39j2m0b43e9pbw40acmuh6ysa365alaefkf5967ggar9528i6rahat638mlb3788ara0bz70d1o7id3te56bnaignj0264b5n7g88ioa140pgu0cyicj71n8vat3kmrap232b",
+        "xn--supercat---api-------ue45a9hq2aeh0m56djybte51xdupzraja917gckh9te2a70glg561g75elsm1jmmqa297fea4ir4hla73gz1eju8cz6hr4jjw9a30ekpbzy0ab5tjtysa705apafkf4584ggaq9594iprahas714mkb2987ara9a029cxt4ins6botzbc6bvagnj2463bug5gc2ioa556oou9bbhcy80e1s8b0vaj2kgraz223b",
       "asmdksmklcmdsk-寻找-lmklamdkjqdenakjc-njkqelnuiconwerj-ksfnvcslkjdmc-jweasjkndjk-sandkjasnjxksakjkxnjaksn适合的-xj-kqwnjkxnqjas-nxsjkanxjksnjxansjk-cnajskn-cjkaxjksn-kxjasnjkxansjk-xnasjkxnksaj-cnjkdcnjksdncjsdnjcsdjkc-nmckj脚本":
         "xn--asmdksmklcmdsk--lmklamdkjqdenakjc-njkqelnuiconwerj-ksfnvcslkjdmc-jweasjkndjk-sandkjasnjxksakjkxnjaksn-xj-kqwnjkxnqjas-nxsjkanxjksnjxansjk-cnajskn-cjkaxjksn-kxjasnjkxansjk-xnasjkxnksaj-cnjkdcnjksdncjsdnjcsdjkc-nmckj-0g768an264bok8jtmrh2e00as1gp9lgw",
     };
